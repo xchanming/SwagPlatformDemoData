d2eef31cb9446097ab9af2745ede10853452bc42
gxs <lyricsong47@gmail.com>
2025-01-04 00:26:12 +0800

    init

--- src/DataProvider/CategoryProvider.php
+++ src/DataProvider/CategoryProvider.php
@@ -17,6 +17,7 @@ use Cicada\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Cicada\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Cicada\Core\Framework\Log\Package;
 use Doctrine\DBAL\Connection;
+use Swag\PlatformDemoData\Resources\helper\DbHelper;
 use Swag\PlatformDemoData\Resources\helper\TranslationHelper;
 
 #[Package('services-settings')]
@@ -31,6 +32,8 @@ class CategoryProvider extends DemoDataProvider
 
     private TranslationHelper $translationHelper;
 
+    private DbHelper $dbHelper;
+
     /**
      * @param EntityRepository<CategoryCollection> $categoryRepository
      */
@@ -39,6 +42,7 @@ class CategoryProvider extends DemoDataProvider
         $this->categoryRepository = $categoryRepository;
         $this->connection = $connection;
         $this->translationHelper = new TranslationHelper($connection);
+        $this->dbHelper = new DbHelper($connection);
     }
 
     public function getAction(): string
@@ -276,6 +280,189 @@ class CategoryProvider extends DemoDataProvider
                             'zh-CN' => '页面',
                             'en-GB' => 'Pages',
                         ]),
+                    ],
+                    [
+                        'id' => '01942ce041de71cfbb175364b166dd4d',
+                        'cmsPageId' => $cmsPageId,
+                        'active' => true,
+                        'displayNestedProducts' => true,
+                        'visible' => false,
+                        'type' => 'page',
+                        'afterCategoryId' => '251448b91bc742de85643f5fccd89051',
+                        'name' => $this->translationHelper->adjustTranslations([
+                            'zh-CN' => 'Footer',
+                            'en-GB' => 'Footer',
+                        ]),
+                        'footerSalesChannels' => [
+                            [
+                                'id' => $this->dbHelper->getStorefrontSalesChannel(),
+                            ],
+                        ],
+                        'children' => [
+                            [
+                                'id' => '01942ce52907722cb7a1c6028669d370',
+                                'cmsPageId' => $cmsPageId,
+                                'active' => true,
+                                'displayNestedProducts' => true,
+                                'visible' => true,
+                                'type' => 'page',
+                                'name' => $this->translationHelper->adjustTranslations([
+                                    'zh-CN' => '其他链接',
+                                    'en-GB' => 'Other links',
+                                ]),
+                                'children' => [
+                                    [
+                                        'id' => '01942cef6d9071b88d31d7ddd7099e5d',
+                                        'cmsPageId' => $cmsPageId,
+                                        'active' => true,
+                                        'displayNestedProducts' => true,
+                                        'visible' => true,
+                                        'type' => 'page',
+                                        'name' => $this->translationHelper->adjustTranslations([
+                                            'zh-CN' => '源码下载',
+                                            'en-GB' => 'Download',
+                                        ]),
+                                    ],
+                                    [
+                                        'id' => '01942cf0a59670a9becc651476cf3563',
+                                        'cmsPageId' => $cmsPageId,
+                                        'active' => true,
+                                        'displayNestedProducts' => true,
+                                        'visible' => true,
+                                        'type' => 'page',
+                                        'afterCategoryId' => '01942cef6d9071b88d31d7ddd7099e5d',
+                                        'name' => $this->translationHelper->adjustTranslations([
+                                            'zh-CN' => '帮助文档',
+                                            'en-GB' => 'Documentation',
+                                        ]),
+                                    ],
+                                    [
+                                        'id' => '01942cf1733873baa3f0840c98cd4ff9',
+                                        'cmsPageId' => $cmsPageId,
+                                        'active' => true,
+                                        'displayNestedProducts' => true,
+                                        'visible' => true,
+                                        'type' => 'page',
+                                        'afterCategoryId' => '01942cf0a59670a9becc651476cf3563',
+                                        'name' => $this->translationHelper->adjustTranslations([
+                                            'zh-CN' => '定制合作',
+                                            'en-GB' => 'Customized collaboration',
+                                        ]),
+                                    ],
+                                    [
+                                        'id' => '01942cf2dc5270969eb5e288a2622e02',
+                                        'cmsPageId' => $cmsPageId,
+                                        'active' => true,
+                                        'displayNestedProducts' => true,
+                                        'visible' => true,
+                                        'type' => 'page',
+                                        'afterCategoryId' => '01942cf1733873baa3f0840c98cd4ff9',
+                                        'name' => $this->translationHelper->adjustTranslations([
+                                            'zh-CN' => '问答社区',
+                                            'en-GB' => 'Q&A community',
+                                        ]),
+                                    ],
+                                ],
+                            ],
+                            [
+                                'id' => '01942cebe8c97092a471f1bd60f8beb9',
+                                'cmsPageId' => $cmsPageId,
+                                'active' => true,
+                                'displayNestedProducts' => true,
+                                'visible' => true,
+                                'type' => 'page',
+                                'afterCategoryId' => '01942ce52907722cb7a1c6028669d370',
+                                'name' => $this->translationHelper->adjustTranslations([
+                                    'zh-CN' => '关于我们',
+                                    'en-GB' => 'About Us',
+                                ]),
+                                'children' => [
+                                    [
+                                        'id' => '01942cf3bfe071a7951dbd3ea7ed98ef',
+                                        'cmsPageId' => $cmsPageId,
+                                        'active' => true,
+                                        'displayNestedProducts' => true,
+                                        'visible' => true,
+                                        'type' => 'page',
+                                        'name' => $this->translationHelper->adjustTranslations([
+                                            'zh-CN' => '合作伙伴',
+                                            'en-GB' => 'partner',
+                                        ]),
+                                    ],
+                                    [
+                                        'id' => '01942cf46b90719b958f9803433cf434',
+                                        'cmsPageId' => $cmsPageId,
+                                        'active' => true,
+                                        'displayNestedProducts' => true,
+                                        'visible' => true,
+                                        'type' => 'page',
+                                        'afterCategoryId' => '01942cf3bfe071a7951dbd3ea7ed98ef',
+                                        'name' => $this->translationHelper->adjustTranslations([
+                                            'zh-CN' => '联系我们',
+                                            'en-GB' => 'Contact Us',
+                                        ]),
+                                    ],
+                                    [
+                                        'id' => '01942cf51b49701689690cbb1ea41911',
+                                        'cmsPageId' => $cmsPageId,
+                                        'active' => true,
+                                        'displayNestedProducts' => true,
+                                        'visible' => true,
+                                        'type' => 'page',
+                                        'afterCategoryId' => '01942cf46b90719b958f9803433cf434',
+                                        'name' => $this->translationHelper->adjustTranslations([
+                                            'zh-CN' => '加入我们',
+                                            'en-GB' => 'Join Us',
+                                        ]),
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    [
+                        'id' => '01942cf93310710d9faf4a4d76e05a9c',
+                        'cmsPageId' => $cmsPageId,
+                        'active' => true,
+                        'displayNestedProducts' => true,
+                        'visible' => false,
+                        'type' => 'page',
+                        'afterCategoryId' => '01942ce041de71cfbb175364b166dd4d',
+                        'name' => $this->translationHelper->adjustTranslations([
+                            'zh-CN' => 'Footer',
+                            'en-GB' => 'Footer',
+                        ]),
+                        'serviceSalesChannels' => [
+                            [
+                                'id' => $this->dbHelper->getStorefrontSalesChannel(),
+                            ],
+                        ],
+                        'children' => [
+                            [
+                                'id' => '01942cf99e0e713ea31d4fdfe5b385bd',
+                                'cmsPageId' => $cmsPageId,
+                                'active' => true,
+                                'displayNestedProducts' => true,
+                                'visible' => true,
+                                'type' => 'page',
+                                'name' => $this->translationHelper->adjustTranslations([
+                                    'zh-CN' => '条款与条件',
+                                    'en-GB' => 'Terms & Conditions',
+                                ]),
+                            ],
+                            [
+                                'id' => '01942cfb468c735293b76d34314c909e',
+                                'cmsPageId' => $cmsPageId,
+                                'active' => true,
+                                'displayNestedProducts' => true,
+                                'visible' => true,
+                                'type' => 'page',
+                                'afterCategoryId' => '01942cf99e0e713ea31d4fdfe5b385bd',
+                                'name' => $this->translationHelper->adjustTranslations([
+                                    'zh-CN' => '隐私协议',
+                                    'en-GB' => 'Privacy',
+                                ]),
+                            ],
+                        ],
                     ],
                 ],
             ],
